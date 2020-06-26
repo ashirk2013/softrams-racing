5f179596f8fa1f606ef669afc779b98464301b4c
Aaron Shirk <ashirk2013@gmail.com>
2020-06-25 20:50:20 -0400

    initial commit

---
+++ src/app/member-details/member-details.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AppService } from '../app.service';
+import { Router } from '@angular/router';
+
+// This interface may be useful in the times ahead...
+interface Member {
+  firstName: string;
+  lastName: string;
+  jobTitle: string;
+  team: string;
+  status: string;
+}
+
+@Component({
+  selector: 'app-member-details',
+  templateUrl: './member-details.component.html',
+  styleUrls: ['./member-details.component.css']
+})
+export class MemberDetailsComponent implements OnInit, OnChanges {
+  memberModel: Member;
+  memberForm: FormGroup;
+  submitted = false;
+  alertType: String;
+  alertMessage: String;
+  teams = [];
+
+  constructor(private fb: FormBuilder, private appService: AppService, private router: Router) {
+    this.memberForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      jobTitle: new FormControl('', Validators.required),
+      team: new FormControl('', Validators.required),
+      status: new FormControl('', Validators.required)
+    });
+  }
+
+  ngOnInit() {
+    this.appService.getTeams().subscribe(teams => this.teams = teams);
+  }
+
+  ngOnChanges() {}
+
+  onSubmit() {
+    this.memberModel = this.memberForm.value;
+    console.log(this.memberModel);
+  }
+}
